c50ef98051e2495e33880cf239415f9a0c5ebe8e
jake-87 <jakehfreebsd@gmail.com>
2022-01-07 20:48:21 +1100

    fortran - broken due to cpu%mem(0)

--- implementations/fortran-vm/src/main.f90
+++ implementations/fortran-vm/src/main.f90
@@ -1,4 +1,15 @@
 program main
+    use parser
     implicit none
+    character (len=100) :: filepath, memory
+    integer(kind = 8) :: imemory
+    if (command_argument_count() /= 2) then
+        print *, "Error: Two arguments needed; ./output <filepath> <memory size>"
+        call EXIT(1)
+    end if
+    call get_command_argument(1, filepath)
+    call get_command_argument(2, memory)
+    read(memory, *) imemory
+    call parse(filepath, imemory)
     print *, "Hello World"
 end program main
