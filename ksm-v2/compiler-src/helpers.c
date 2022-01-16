043db45a4f8124c659dcd3575e2ea2115426f07f
jake-87 <jakehfreebsd@gmail.com>
2022-01-16 22:04:03 +1100

    comments

--- ksm-v2/compiler-src/helpers.c
+++ ksm-v2/compiler-src/helpers.c
@@ -7,6 +7,19 @@ void flusher() {
 }
 void print_hex(int64_t a) {
     printf("%c0x%lx\n", a < 0 ? '-' : ' ', (uint64_t) labs(a));
+    /*
+        Broken down:
+        
+        %c : a < 0 ? '-' : ' '
+        This prints either a minus sign if a is negative, or a space if it is not.
+
+        0x : prints "0x"
+
+        %lx : (uint64_t) labs(a)
+        This prints the positive representation of "a", cast to a uint64_t.
+
+        \n : prints '\n
+    */
     flusher();
 }
 void print_int(int64_t a) {
